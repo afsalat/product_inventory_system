fa323e2cb3c5a094059656e2f14c491f0adc7478
AFSAL_AT <afsalat9@gmail.com>
2024-08-29 23:36:33 +0530

    backend is modified  -  trying to implement tokens based login - 30 - 8 - 24

--- product_inventory_system_api/members/models.py
+++ product_inventory_system_api/members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class Member(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
--- product_inventory_system_api/product_inventory_system/settings.py
+++ product_inventory_system_api/product_inventory_system/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'products',
-    'user',
+    'members',
     'corsheaders',
     'rest_framework_simplejwt',
 ]
