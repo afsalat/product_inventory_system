0d43f477dd5a13f0303994b9ccc5538f1c8bbaf6
AFSAL_AT <afsalat9@gmail.com>
2024-09-02 22:58:53 +0530

    beckend is modified - added token api

--- product_inventory_system_api/members/models.py
+++ product_inventory_system_api/members/models.py
@@ -1,6 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
 
+class MemberManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
 
 class Member(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
@@ -8,6 +23,22 @@ class Member(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='member_groups',  # Unique related_name
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='member_permissions',  # Unique related_name
+        blank=True,
+    )
+
+    objects = MemberManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
