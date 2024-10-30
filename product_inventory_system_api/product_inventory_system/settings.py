20e8d6dba7f014c711e3d316942759e859c8c390
AFSAL_AT <afsalat9@gmail.com>
2024-10-30 21:58:20 +0530

    fix - hosting issue fixed

--- product_inventory_system_api/product_inventory_system/settings.py
+++ product_inventory_system_api/product_inventory_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0i+qlnj!az&q^nvlsx#rgyn19v47^bi_dt1-+2ulf(t9w+05j#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['product-inventory-system', 'localhost', '127.0.0.1']
 
 APPEND_SLASH = False
 
