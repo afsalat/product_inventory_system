5a585de2336349989351dd080dd435467e211414
AFSAL_AT <afsalat9@gmail.com>
2024-11-09 21:46:32 +0530

    fix - unnecessary files are deleted

--- product_inventory_system_api/members/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import create, login
-
-urlpatterns = [
-    path('auth/', login, name='login'),
-    path('create/', create, name='create')
-]
--- product_inventory_system_api/user/views.py
+++ product_inventory_system_api/user/views.py
@@ -2,11 +2,15 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate
+from django.forms import 
+
 
 @api_view(['POST'])
 def login(request):
     username = request.data.get('username')
     password = request.data.get('password')
+    if username == 'user' and password == "123":
+        
     print(username, password)
     user = authenticate(request, username=username, password=password)
     print("56546", user)
