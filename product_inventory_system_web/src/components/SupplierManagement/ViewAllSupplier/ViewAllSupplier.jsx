e1e9845c330c9a1162f9b3a78e3e570c94f2d9e8
AFSAL_AT <afsalat9@gmail.com>
2024-12-11 23:13:07 +0530

    feat - frontend : table designing and api calling

--- product_inventory_system_web/src/components/SupplierManagement/ViewAllSupplier/ViewAllSupplier.jsx
+++ product_inventory_system_web/src/components/SupplierManagement/ViewAllSupplier/ViewAllSupplier.jsx
@@ -1,10 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import './ViewAllSupplier.css';
+import axios from 'axios';
 
 
 function ViewAllSupplier() {
 
-    [supplier, setSupplier] =  useState([])
+    const [supplier, setSupplier] =  useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPage] = useState(1);
+    const [pageSize, setPageSize] = useState(5);
+    const [success, setSuccess] = useState('');
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchSuppliers = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/get-all-supplier/', {
+                    params: {
+                        page: page,
+                        page_size: pageSize
+                    }
+                });
+
+                if (response.data && Array.isArray(response.data.results)) {
+                    setSupplier(response.data.results);
+                    setTotalPage(Math.ceil(response.data.count / pageSize));
+                } else {
+                    setSupplier([]);
+                    setTotalPage(0);
+                }
+            } catch (error) {
+                console.error('There was an error fetching the products!', error);
+                setError('Unable to fetch products. Please try again later.');
+            }
+        };
+        fetchSuppliers();
+    }, [page, pageSize])
 
 
     return (
